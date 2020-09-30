b310a24272cff59e9122cc08145d6af429e4c825
Sandra Shumanteva Kaevska <sandra@shumantevi.mk>
2020-09-30 09:33:28 +0200

    filters

--- script.js
+++ script.js
@@ -1,75 +1,81 @@
-const cardContainer = document.getElementById('card-container')
-const card = document.getElementById('card')
-const restoInfo = document.getElementById('api-data')
-
-
-
+const cardContainer = document.getElementById("card-container");
+const card = document.getElementById("card");
+const restoInfo = document.getElementById("api-data");
+let restaurantList = [];
+let priceRange = null;
+let goodReviewOnly = null;
 
 const fetchZomato = () => {
-  const apiKey = "66748116ada83a96ea0d8e2b3c763360" //Peggy
-  const cityID = 91 //Dublin
-  const cusineID = 82 //pizza
-  const apiURL = `https://developers.zomato.com/api/v2.1/search?entity_id=${cityID}&entity_type=city&cuisines=${cusineID}`
+  const apiKey = "66748116ada83a96ea0d8e2b3c763360"; //Peggy
+  const cityID = 91; //Dublin
+  const cusineID = 82; //pizza
+  const apiURL = `https://developers.zomato.com/api/v2.1/search?entity_id=${cityID}&entity_type=city&cuisines=${cusineID}`;
 
   fetch(apiURL, { headers: { "user-key": apiKey } })
     .then((response) => {
       if (!response.ok) {
-        throw Error(response.statusText)
+        throw Error(response.statusText);
       }
-      return response.json()
+      return response.json();
     })
-    .then((json) => {
-      
-      console.log(json)
-      json.restaurants.forEach(place => {
-          
-        console.log(place.restaurant.price_range);
+    .then((response) => {
+      restaurantList = response.restaurants;
 
-        cardContainer.innerHTML += `
-        <article class="card" id="card"> 
-        
-          <img class="pic" src="${place.restaurant.thumb}"/>
-          <h4>${place.restaurant.name}</h4>
-          <p>Average cost for 2: ${place.restaurant.average_cost_for_two}</p>
-          <p>Neightborhood: ${place.restaurant.location.locality}</p>
-          <p>Online Delivery: ${place.restaurant.has_online_delivery}</p>
-          <p>Table Booking: ${place.restaurant.has_table_booking}</p>
-          <p>Aggregate Rating: ${place.restaurant.user_rating.aggregate_rating}</p>
-          <p>Price Range: ${place.restaurant.price_range}</p>
-         
-          <p>Address: ${place.restaurant.location.address}</p>
-        
-        </article>`
-      })
-    })
-}
-
-fetchZomato()
-
-//  function to filter by price range
-const checkBox = document.getElementsByName('price-range')
-const getValue = (radio) => {
-  fetchZomato(radio.value)
-}
-
-
-// checkBox.addEventListener('click', () => {
-  
-  
-//   console.log(checkedValue)
-// })
-
-/*
-const getValue = (radio) => {
-  weatherToday(radio.value);
-  forcastWeather(radio.value);
+      printRestorants(restaurantList);
+    });
 };
 
-const filteredArr = newWeatherForcastArr.filter((item) =>
-        item["dt_txt"].includes("12:00:00")
-      );
+fetchZomato();
 
-*/      
+// Filter
+const onPriceRangeChange = (target) => {
+  priceRange = target.value;
+  printRestorants(filterList());
+};
+const onReviewChange = (target) => {
+  goodReviewOnly = target.checked;
+  printRestorants(filterList());
+};
+
+const filterList = () => {
+  let filteredRestaurants = restaurantList;
+
+  if (priceRange) {
+    filteredRestaurants = filteredRestaurants.filter(
+      (item) => item.restaurant.price_range == priceRange
+    );
+  }
+
+  if (goodReviewOnly) {
+    filteredRestaurants = filteredRestaurants.filter(
+      (item) => item.restaurant.user_rating.aggregate_rating >= 4.1
+    );
+  }
+  return filteredRestaurants;
+};
+
+const printRestorants = (restaurants) => {
+  cardContainer.innerHTML = "";
+
+  restaurants.forEach((place) => {
+    cardContainer.innerHTML += `
+    <article class="card" id="card"> 
+    
+      <img class="pic" src="${place.restaurant.thumb}"/>
+      <h4>${place.restaurant.name}</h4>
+      <p>Average cost for two: ${place.restaurant.average_cost_for_two} ${place.restaurant.currency}</p>
+      <p>Neightborhood: ${place.restaurant.location.locality}</p>
+      <p>Online Delivery: ${place.restaurant.has_online_delivery}</p>
+      <p>Table Booking: ${place.restaurant.has_table_booking}</p>
+      <p>Aggregate Rating: ${place.restaurant.user_rating.aggregate_rating}</p>
+      <p>Price Range: ${place.restaurant.price_range}</p>
+     
+      <p>Address: ${place.restaurant.location.address}</p>
+    
+    </article>`;
+  });
+};
+
 /* Build a Food Review website
 Pizza restaurants in Dublin
 in the style of Deliveroo
@@ -102,6 +108,7 @@ Make it so your users can choose to only show resturants which have delivery (ha
 
 Filter on reviews
 Make it so your users can choose to only show restaurants with good ratings.
+
 
 Make the page responsive
 To work on mobile, desktop or tablet.
