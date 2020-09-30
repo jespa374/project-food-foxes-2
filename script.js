41d86b17be5d772a5168eb2c03936521c1f26e93
JESSICA PANDITHA <JESSICA.PANDITHA@GMAIL.COM>
2020-09-30 11:14:56 +0200

    sortonpric API

--- script.js
+++ script.js
@@ -54,8 +54,14 @@ const getValue = (radio) => {
 
 //function sort price
 
+let restaurantList = [];
+let sortToLowest = null;
+let sortTohighest = null;
 
-
+const sortFromLow = (event) => {
+  sortToLowest = target.value;
+  sortbyLowPrice(filterList());
+};
 
 const sortbyLowPrice = () => {
   const cityID = 91 //Dublin
@@ -63,8 +69,6 @@ const sortbyLowPrice = () => {
   const API_URL_LOW_PRICE = `https://developers.zomato.com/api/v2.1/search?entity_id=${cityID}&entity_type=city&cuisines=${cusineID}&sort=cost&order=asc`;
   const apiKey = "66748116ada83a96ea0d8e2b3c763360" //Peggy
   
-
-
   fetch(API_URL_LOW_PRICE, { headers: { "user-key": apiKey } })
     .then((response) => {
       if (!response.ok) {
@@ -73,35 +77,39 @@ const sortbyLowPrice = () => {
       return response.json()
     })
     .then((json) => {
-      
-      console.log(json)
+      restaurantList = json.restaurants;
       json.restaurants.forEach(place => {
           
         console.log(place.restaurant.name);
         console.log(place.restaurant.average_cost_for_two);
         console.log(place.restaurant.price_range);
 
-        /*cardContainer.innerHTML += `
-        <article class="card" id="card"> 
-        
-          <img class="pic" src="${place.restaurant.thumb}"/>
-          <h4>${place.restaurant.name}</h4>
-          <p>Average cost for 2: ${place.restaurant.average_cost_for_two}</p>
-          <p>Neightborhood: ${place.restaurant.location.locality}</p>
-          <p>Online Delivery: ${place.restaurant.has_online_delivery}</p>
-          <p>Table Booking: ${place.restaurant.has_table_booking}</p>
-          <p>Aggregate Rating: ${place.restaurant.user_rating.aggregate_rating}</p>
-          <p>Price Range: ${place.restaurant.price_range}</p>
-         
-          <p>Address: ${place.restaurant.location.address}</p>
-        
-        </article>`*/
+        const printRestaurants = (restaurants) => {
+        cardContainer.innerHTML += ""
+        restaurants.forEach((place) => {
+          cardContainer.innerHTML +=
+          `<article class="card" id="card"> 
+            <img class="pic" src="${place.restaurant.thumb}"/>
+            <h4>${place.restaurant.name}</h4>
+            <p>Average cost for 2: ${place.restaurant.average_cost_for_two}</p>
+            <p>Neightborhood: ${place.restaurant.location.locality}</p>
+            <p>Online Delivery: ${place.restaurant.has_online_delivery}</p>
+            <p>Table Booking: ${place.restaurant.has_table_booking}</p>
+            <p>Aggregate Rating: ${place.restaurant.user_rating.aggregate_rating}</p>
+            <p>Price Range: ${place.restaurant.price_range}</p>
+            <p>Address: ${place.restaurant.location.address}</p>
+         </article>`
+        })
+      }
       })
     })
   }
-  const sortButton = document.getElementById('sort-button');
-  roomCard.addEventListener('click', sortbyLowPrice);
-  sortbyLowPrice();
+
+  const sortFromHigh = (event) => {
+    sortByHighPrice = target.value;
+    sortbyLowP(filterList());
+  };
+  
 
   const sortbyHighPrice = () => {
     const apiKey = "66748116ada83a96ea0d8e2b3c763360" //Peggy
@@ -121,14 +129,15 @@ const sortbyLowPrice = () => {
         
         console.log(json)
         json.restaurants.forEach(place => {
-            
+         
           console.log(place.restaurant.name);
           console.log(place.restaurant.average_cost_for_two);
           console.log(place.restaurant.price_range);
-  
-          /*cardContainer.innerHTML += `
-          <article class="card" id="card"> 
-          
+          const printRestaurants = (restaurants) => {
+          cardContainer.innerHTML += "";
+          restaurants.forEach((place) => {
+            cardContainer.innerHTML +=
+          `<article class="card" id="card">
             <img class="pic" src="${place.restaurant.thumb}"/>
             <h4>${place.restaurant.name}</h4>
             <p>Average cost for 2: ${place.restaurant.average_cost_for_two}</p>
@@ -137,62 +146,37 @@ const sortbyLowPrice = () => {
             <p>Table Booking: ${place.restaurant.has_table_booking}</p>
             <p>Aggregate Rating: ${place.restaurant.user_rating.aggregate_rating}</p>
             <p>Price Range: ${place.restaurant.price_range}</p>
-           
             <p>Address: ${place.restaurant.location.address}</p>
-          
-          </article>`*/
+          </article>`
         })
+        }
       })
-    }
-    sortbyHighPrice();
-    roomCard.addEventListener('click', sortbyLowPrice);
+    })
+
+    
+   
+    //roomCard.addEventListener('click', sortbyLowPrice);
 
 //sort on price code suggestion
 
 
-    
-/* Build a Food Review website
-Pizza restaurants in Dublin
-in the style of Deliveroo
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
 
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
 
-// Step 1
-Each member of your team should clone the repo for your team.
-
-
-// Step 3
-Divide into pairs or groups of three and choose freely from the selection of features below. The groups will work separatley on features in their own "feature branch" and we will help you to merge this into the master branch before the demo. Of course; have communication within the group and feel free to merge and commit code when you feel that the time is right. Help each-other and share knowledge in the team is always encouraged.
-
-// Features
-Below are suggestions for features for your site. You're free to choose the ones which are interesting to your team to implement:
-
-Implement the randomly given design.
-You don't need to implement all sections (if there's any that is not relevant to your site) but try and copy the page look and feel and design details. Feel free to make text, logo and other things your own – it's a tribute, not an exact copy.
-
-Filter on price range 
-Add so that your users can filter restaurants based on different price ranges.
-
-Sort on price
-Add so that your users can sort the resturants on price, cheap to expensive using the average_cost_for_two parameter.
-
-List reviews
-List the 3 latest reviews for each restaurant.
-
-Filter on delivery options
-Make it so your users can choose to only show resturants which have delivery (has_online_delivery) or can be booked in advance (has_table_booking).
-
-Filter on reviews
-Make it so your users can choose to only show restaurants with good ratings.
-
-Make the page responsive
-To work on mobile, desktop or tablet.
-
-Feel free to challenge yourself and team to come up with more or other features! This is your group project – you decided.
-
-
-// Feature freeze
-There's a thing called feature freeze., our feature freeze will be 2 hours before the demo. That's when it's time for you to merge your feature branches into the master branch. Our teachers will have a bit of a though time, since it's guaranteed there will be conflicts in your code (which happens when the same code is edited in two different branches). So expect merge conflicts at this point!
-
-Once all the feature branches are merged into master, you're free to try and make smaller changes to your site and fix bugs - but be careful to make sure it'll be demoable.
-
-*/
